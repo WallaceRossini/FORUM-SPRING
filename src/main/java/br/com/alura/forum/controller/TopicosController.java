1c4f6ae06a9c662570ed56e16a64c00e2f750635
Wallace Rossini <wallace.costa@compasso.com.br>
2022-08-08 23:57:32 -0300

    feat: detailing the directions

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
@@ -53,6 +55,13 @@ public class TopicosController {
 
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 
+	}
+	
+	@GetMapping("/{id}")
+	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getReferenceById(id);
+		
+		return new DetalhesDoTopicoDto(topico);
 	}
 
 }
